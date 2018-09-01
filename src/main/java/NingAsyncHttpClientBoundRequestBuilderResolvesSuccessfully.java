fda9be8eeae10f6c49423d33ed552d158c2ab726
Billy Fisher <billy.fisher@nike.com>
2018-08-31 23:56:36 -0700

    Initial commit that shows that `./gradlew clean build` does succeed when importing `com.ning.http.client.AsyncHttpClient.BoundRequestBuilder` from a java file.

---
+++ src/main/java/NingAsyncHttpClientBoundRequestBuilderResolvesSuccessfully.java
@@ -0,0 +1,5 @@
+import com.ning.http.client.AsyncHttpClient.BoundRequestBuilder;
+
+public class NingAsyncHttpClientBoundRequestBuilderResolvesSuccessfully {
+  private BoundRequestBuilder builder;
+}
