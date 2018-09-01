17aa37eb50cbbb48bf16bac7ec9e0cd50d9c0d19
Billy Fisher <billy.fisher@nike.com>
2018-09-01 11:40:14 -0700

    Comment out offending Scala import so that build succeeds again with java example import.

--- src/main/scala/NingAsyncHttpClientBoundRequestBuilderFailsToResolve.scala
+++ src/main/scala/NingAsyncHttpClientBoundRequestBuilderFailsToResolve.scala
@@ -1,5 +1,7 @@
+/*
 import com.ning.http.client.AsyncHttpClient.BoundRequestBuilder
 
 class NingAsyncHttpClientBoundRequestBuilderFailsToResolve {
   private var builder: BoundRequestBuilder
 }
+*/
